2552ec843c8be78d76d6730b5a86ba26435d414b
Sean Murphy <sean@iamseanmurphy.com>
2024-10-18 14:30:33 +0000

    6.2.6

--- login-page-styler/loginPageStylerAdminUrl.php
+++ login-page-styler/loginPageStylerAdminUrl.php
@@ -1,11 +1,8 @@
-<?php 
+<?php
 
 if (!defined('ABSPATH')) {
     exit;
 }
-
-// Plugin activation hook (if needed)
-// register_activation_hook(__FILE__, 'your_activation_function');
 
 /**
  * Deactivate wp-login and activate the new URL
@@ -29,13 +26,21 @@ function lps_plugin_on_page_loaded() {
 }
 
 /**
- * Take care of the redirections
+ * Handle redirections and special cases like logout
  */
 add_action('wp_loaded', 'lps_redirect_page', 1);
 function lps_redirect_page() {
     global $pagenow, $lps_is_login;
 
     if (get_option('lps_new_login_url')) {
+        // Check if the user is trying to log out
+        if (isset($_GET['action']) && $_GET['action'] === 'logout') {
+            // Let WordPress handle the logout and redirection
+            wp_logout();
+            wp_safe_redirect(home_url());
+            exit;
+        }
+
         if (! (isset($_GET['action']) && isset($_POST['post_password']) && $_GET['action'] == 'postpass')) {
             if ($lps_is_login) {
                 nocache_headers();
@@ -90,37 +95,9 @@ function lps_filter_login_url($url, $path, $scheme, $blog_id) {
     return $url;
 }
 
-/**
- * If URL sent contains wp-login.php,
- * recreate an url with the custom link instead
- */
-function lps_filter_login($url) {
-    if (strpos($url, 'wp-login.php?action=postpass') !== false) {
-        return $url;
-    }
-
-    if (strpos($url, 'wp-login.php') && strpos(wp_get_referer(), 'wp-login.php') === false) {
-        $args = explode('?', $url);
-        $custom_login_url = get_option('lps_new_login_url', '');
-        
-        if (isset($args[1])) {
-            parse_str($args[1], $args);
-            $url = add_query_arg($args, get_site_url() . "/" . $custom_login_url);
-        } else {
-            $url = get_site_url() . "/" . $custom_login_url;
-        }
-    }
-
-    return $url;
-}
-
-/**
- * Filter the login URL to use the custom login link instead
- */
 add_filter('login_url', 'lps_login_url', 10, 3);
 function lps_login_url($login_url, $redirect, $force_reauth) {
     if (get_option('lps_new_login_url')) {
-        // If the URL is install.php, hide the login
         if (mb_strpos($_SERVER['REQUEST_URI'], "wp-admin/install.php")) {
             return admin_url();
         }
@@ -148,4 +125,3 @@ function lps_login_url($login_url, $redirect, $force_reauth) {
     return $login_url;
 }
 
-?>
